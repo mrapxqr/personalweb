5e299a214073bb5199feb59cece8b581595b424f
UTA <123853140+mrapxqr@users.noreply.github.com>
2023-08-04 18:53:51 +0700

    Add files via upload
    update

--- main.js/script.js
+++ main.js/script.js
@@ -17,3 +17,9 @@ window.onbeforeunload = () => {
         form.reset();
     }
 };
+
+// toggle switch light/dark mode //
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
