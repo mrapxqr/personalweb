60010ca43ecafb147056b30e6ecb6e59e8a2e7e1
UTA <123853140+mrapxqr@users.noreply.github.com>
2023-08-13 12:26:58 +0700

    Add files via upload

--- main.js/script.js
+++ main.js/script.js
@@ -23,3 +23,22 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+//  script disable inspect //
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
